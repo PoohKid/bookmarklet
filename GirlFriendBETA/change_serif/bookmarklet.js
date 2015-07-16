88581ba37c7a0b57c0e34db681c86a873eca8877
PoohKid <poohkid@gmail.com>
2015-07-17 02:18:03 +0900

    Update bookmarklet.js

--- GirlFriendBETA/change_serif/bookmarklet.js
+++ GirlFriendBETA/change_serif/bookmarklet.js
@@ -1,4 +1,13 @@
-var div = document.querySelector('div.js_topGirlComment.topGirlComment');
+var swipe = null;
+var swipeList = document.querySelectorAll('div.moduleSwipeList');
+for (var i=0; i < swipeList.length; i++) {
+    if (swipeList[i].style.webkitTransform.match(/^translate3d\(0px/)) {
+        swipe = swipeList[i];
+    }
+}
+if (!swipe) return;
+
+var div = swipe.querySelector('div.js_topGirlComment.topGirlComment');
 div.style.opacity = 100;
 var input = document.createElement('input');
 input.type = 'text';
