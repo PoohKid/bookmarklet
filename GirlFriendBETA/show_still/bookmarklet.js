790f7886e6bd7c17b0d73117a768d827eb284155
PoohKid <poohkid@gmail.com>
2015-07-17 00:23:55 +0900

    Update bookmarklet.js

--- GirlFriendBETA/show_still/bookmarklet.js
+++ GirlFriendBETA/show_still/bookmarklet.js
@@ -1,4 +1,7 @@
-document.querySelector('nav.mypageNavi.relative').style.display='none';
-document.querySelector('div#deckSwitch').style.display='none';
-document.querySelector('div#girlsVoiceBtn').style.display='none';
-document.querySelector('div#main').style.display='none';
+document.querySelector('div.js_menuBtn.absolute.z3').style.display='none';
+document.querySelector('section.relative.z2').style.display='none';
+document.querySelector('div.absolute.js_prev.floatLeft.z3.js_fadeout').style.display='none';
+document.querySelector('div.absolute.js_next.floatRight.z3.js_fadeout').style.display='none';
+document.querySelector('div.js_specialBtn').style.display='none';
+document.querySelector('div.js_voiceBtn').style.display='none';
+document.querySelector('div.js_girlChangeBtn.absolute.z3').style.display='none';
