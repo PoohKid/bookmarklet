72d35788e4eb93c96c8a581f00fa68d21a2fed60
PoohKid <poohkid@gmail.com>
2016-03-15 01:19:01 +0900

    images support URL strings

--- images/bookmarklet.js
+++ images/bookmarklet.js
@@ -7,8 +7,16 @@ var divs = document.querySelectorAll('div');
 for (var i = 0; i < divs.length; i++) {
     var bg = document.defaultView.getComputedStyle(divs[i], null).getPropertyValue('background-image').match(/url\((.*)\)/);
     if (bg) {
-        srcs.push(bg[1]);
+        srcs.push(bg[1].replace(/'|"/g,''));
     }
+}
+imgs = getImageURL(document.head.innerHTML);
+for (var i = 0; i < imgs.length; i++) {
+    srcs.push(imgs[i])
+}
+imgs = getImageURL(document.body.innerHTML);
+for (var i = 0; i < imgs.length; i++) {
+    srcs.push(imgs[i])
 }
 removeChildren(document.body);
 removeChildren(document.head);
@@ -21,6 +29,17 @@ for (var i = 0; i < srcs.length; i++) {
     }, false);
     div.appendChild(image);
     document.body.appendChild(div)
+}
+
+function getImageURL(text) {
+    var url_array = text.match(/http[s]?\:\/\/[\w\+\$\;\?\.\%\,\!\#\~\*\/\:\@\&\\\=\_\-]+/g);
+    var images = new Array;
+    for (var i = 0; i < url_array.length; i++) {
+        if (url_array[i].match(/.jpg$|.png$/)) {
+            images.push(url_array[i]);
+        }
+    }
+    return images;
 }
 
 function removeChildren(element) {
